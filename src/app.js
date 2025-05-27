9960beca34ac8d0c6e14ba731bd3f9a2d8ca65b1
dtri17 <155714018+dtri17@users.noreply.github.com>
2025-05-27 21:01:02 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes')
+
+app.use(cors());
 
 // Parse JSON
 app.use(express.json());
