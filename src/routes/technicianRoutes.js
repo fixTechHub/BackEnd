c95fe7d9217a152d7d4cfe6e47abd8e6e07e8033
ttuan9909 <ttanhtuan2003@gmail.com>
2025-06-20 13:10:15 +0700

    cap nhat code moi

--- src/routes/technicianRoutes.js
+++ src/routes/technicianRoutes.js
@@ -3,25 +3,25 @@ const validate = require('../middlewares/validationMiddleware');
 const technicianValidation = require('../validations/technicianValidation');
 const technicianController = require('../controllers/technicianController');
 const router = express.Router();
-const { registerAsTechnician,
-    viewTechnicianProfile,
-    getCertificatesByTechnicianId,
-    viewJobDetails,
-    viewEarningsByBooking,
-    getTechnicianAvailability,
-    updateAvailability,
-    sendQuotation,
-    confirmJobDoneByTechnician } = require('../controllers/technicianController');
+// const { registerAsTechnician,
+//     viewTechnicianProfile,
+//     getCertificatesByTechnicianId,
+//     viewJobDetails,
+//     viewEarningsByBooking,
+//     getTechnicianAvailability,
+//     updateAvailability,
+//     sendQuotation,
+//     confirmJobDoneByTechnician } = require('../controllers/technicianController');
 
 
-router.get('/:technicianId', viewTechnicianProfile);
-router.get('/:technicianId/certificates', getCertificatesByTechnicianId);
-router.get('/:technicianId/earnings', viewEarningsByBooking);
-router.get('/:technicianId/availability', getTechnicianAvailability);
-router.get('/:technicianId/:bookingId', viewJobDetails);
-router.put('/:technicianId/availability', updateAvailability);
-router.post('/register', registerAsTechnician);
-router.post('/send-quotation', sendQuotation);
-router.post('/:bookingId/done', confirmJobDoneByTechnician);
+router.get('/:technicianId', technicianController.viewTechnicianProfile);
+router.get('/:technicianId/certificates', technicianController.getCertificatesByTechnicianId);
+router.get('/:technicianId/earnings', technicianController.viewEarningsByBooking);
+router.get('/:technicianId/availability', technicianController.getTechnicianAvailability);
+router.get('/:technicianId/:bookingId', technicianController.viewJobDetails);
+router.put('/:technicianId/availability', technicianController.updateAvailability);
+router.post('/register', technicianController.registerAsTechnician);
+router.post('/send-quotation', technicianController.sendQuotation);
+router.post('/:bookingId/done', technicianController.confirmJobDoneByTechnician);
 
 module.exports = router;
