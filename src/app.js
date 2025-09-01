fcc95ebc8fa9eb26760f35ad21dce009e35b0aa4
Quoc Dat <datquocnl@gmail.com>
2025-09-01 18:46:17 +0700

    fix code

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(
         ],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Session-Type'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
         exposedHeaders: ['set-cookie']
     })
 );
